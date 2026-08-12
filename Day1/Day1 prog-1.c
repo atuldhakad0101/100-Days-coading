93687142d3b48e6f76f77365047beab2684b35c5
atuldhakad0101 <15atul.dhakad15@gmail.com>
2026-08-12 20:53:37 +0530

    Add program description comment to Day1 prog-1.c
    Added a comment to describe the program's purpose.

--- Day1/Day1 prog-1.c
+++ Day1/Day1 prog-1.c
@@ -1,4 +1,5 @@
-
+/*Q1: Write a program to input two numbers and display their sum.
+	
 #include<stdio.h>
 int main()
 {
@@ -11,3 +12,8 @@ int main()
 	printf("sum=%d",sum);
 	return 0;
 }
+
+atul@atul-G15-1111:~/Desktop/c-language$ ./d1p1.out
+helllo!Enter two numbers:25 5
+a=25 b=5
+sum=30
