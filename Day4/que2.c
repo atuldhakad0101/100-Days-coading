4062f3af94ed0e8770072bf3119d4acebd2d2ed3
atuldhakad0101 <15atul.dhakad15@gmail.com>
2026-08-16 16:08:06 +0530

    Update comment for clarity on program purpose

--- Day4/que2.c
+++ Day4/que2.c
@@ -1,4 +1,4 @@
-/*write a program to find and display the sum of the first n natural numbers.*/
+/*Q8: write a program to find and display the sum of the first n natural numbers.*/
 #include<stdio.h>
 int main() {
     int n,sum=0;
